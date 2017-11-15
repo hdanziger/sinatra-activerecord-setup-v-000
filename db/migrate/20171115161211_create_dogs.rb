894e1277b9ef1be00c32f528857c7a4ffc01b72a
Hannah Danziger <hdanziger8@gmail.com>
2017-11-15 16:13:50 +0000

    automatically backed up by learn

--- db/migrate/20171115161211_create_dogs.rb
+++ db/migrate/20171115161211_create_dogs.rb
@@ -1,5 +1,9 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
+  def up #creates a table with columns
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
   end
 
   def down
